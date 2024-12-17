1442a5a65c8349e78e010d924b42ef9d0a7d89bc
dhoouglas <dhoouglas@hotmail.com>
2024-12-17 17:13:27 -0300

    feat: components created for market details screen and map and pin configured on the map

--- nearby/src/app/home.tsx
+++ nearby/src/app/home.tsx
@@ -1,17 +1,32 @@
-import { View, Alert } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, Alert } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
 
 import { api } from "@/services/api";
-import { useEffect, useState } from "react";
+import { colors, fontFamily } from "@/styles/theme";
+
 import { Categories, CategoriesProps } from "@/components/categories";
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
+import { router } from "expo-router";
 
-type MarketsProps = PlaceProps & {};
+// import * as Location from "expo-location";
+
+type MarketsProps = PlaceProps & {
+  latitude: number;
+  longitude: number;
+};
+
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494,
+};
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([]);
   const [category, setCaterogy] = useState("");
   const [markets, setMarkets] = useState<MarketsProps[]>();
+
   async function fetchCategories() {
     try {
       const { data } = await api.get("/categories");
@@ -37,6 +52,20 @@ export default function Home() {
     }
   }
 
+  // Função para pegar a localização atual
+  // async function getCurrentLocation() {
+  //   try {
+  //     const { granted } = await Location.requestForegroundPermissionsAsync();
+
+  //     if (granted) {
+  //       const location = await Location.getCurrentPositionAsync();
+  //       console.log(location);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
   useEffect(() => {
     fetchCategories();
   }, []);
@@ -44,6 +73,7 @@ export default function Home() {
   useEffect(() => {
     fetchMarkets();
   }, [category]);
+
   return (
     <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
       <Categories
@@ -51,6 +81,61 @@ export default function Home() {
         onSelected={setCaterogy}
         selected={category}
       />
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
+
+        {markets?.map((item) => (
+          <Marker
+            key={item.id}
+            identifier={item.id}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude,
+            }}
+            image={require("@/assets/pin.png")}
+          >
+            <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.medium,
+                  }}
+                >
+                  Testando
+                </Text>
+
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.regular,
+                  }}
+                >
+                  Testando
+                </Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
       <Places data={markets} />
     </View>
   );
